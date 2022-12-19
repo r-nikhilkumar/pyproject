9a45cfcf068143d623e1c7ec1fff6606b65e6f0e
Manasvi Gaur <manasvigaur3@gmail.com>
2022-12-19 11:40:50 +0530

    manasvi

--- chr.py
+++ chr.py
@@ -1,3 +1,19 @@
-a=65
-print(ord('a'))
-print(chr(a))
+import mysql.connector
+from mysql.connector import Error
+x=input()
+y=input()
+z=int(input())
+try :
+    connection = mysql.connector.connect(host="localhost",database="k1",user="root",password="109283")
+    score_table="""CREATE TABLE score (first_table varchar(20),last_name varchar(20),score int(100))"""
+    insert_table=f"""INSERT INTO SCORE_TABLE(first_table,last_name,score) VALUES({x},{y},{z})"""
+    cursor =connection.cursor()
+    cursor.execute(insert_table)
+    result= cursor.execute(score_table)
+    connection.commit()
+except Exception:
+    connection.rollback()
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
--- sound.py
+++ sound.py
@@ -1,28 +1,30 @@
-from tkinter import *
-import pygame
 
-
-w = Tk()
-w.title('sound on')
-w.geometry('300x300')
-
-pygame.mixer.init()
-
-
-def play_music():
-    pygame.mixer.music.load("C:\\Users\\Manasvi Gaur\\Desktop\\pyproject\\sound.mp3")
-    pygame.mixer.music.play(loops=3)
-
-sound_btn = PhotoImage(file="C:\\Users\\\Manasvi Gaur\\Desktop\\pyproject\\sounnd button resize.png")
-img_label = Button(w,image=sound_btn,height=19,width=14,command=play_music)
-
-
-img_label.pack(pady=20)
-
-
+# from tkinter import *
+# import pygame
+#
+#
+# w = Tk()
+# w.title('sound on')
+# w.geometry('300x300')
+#
+# pygame.mixer.init()
+#
+#
+# def play_music():
+#     pygame.mixer.music.load("C:\\Users\\Manasvi Gaur\\Desktop\\pyproject\\sound.mp3")
+#     pygame.mixer.music.play(loops=3)
+#
+# sound_btn = PhotoImage(file="C:\\Users\\\Manasvi Gaur\\Desktop\\pyproject\\sounnd button resize.png")
+# img_label = Button(w,image=sound_btn,height=19,width=14,command=play_music)
+#
+#
+# img_label.pack(pady=20)
+#
+#
 # sdButton = Button(w,text='play',font=("Helvetica",23),command=play_music)
- # sdButton.pack(pady=10,padx=1)
-
-
-
-w.mainloop()
+#  sdButton.pack(pady=10,padx=1)
+#
+#
+#
+# w.mainloop()*/
+import my
